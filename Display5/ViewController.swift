2b2070634e4cdee5658a4ba10c088b9e10f04e29
Tony Tran <londontony01"gmail.com>
2018-11-08 22:55:20 +0000

    Initial Commit

---
+++ Display5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Display5
+//
+//  Created by Dung Tran on 08/11/2018.
+//  Copyright © 2018 Dung Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
